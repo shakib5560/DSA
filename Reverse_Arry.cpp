8780593395731f874edb8ef757d7793c781cb449
shakib5560 <dev.shakib6@gmail.com>
2025-07-12 01:57:27 +0600

    Stage 2

--- Reverse_Arry.cpp
+++ Reverse_Arry.cpp
@@ -14,3 +14,30 @@ int main(){
     int b = sizeof(a)/sizeof(int);
     logic(a,b);
 }
+
+
+#With the help of swap
+
+
+#include <iostream>
+using namespace std;
+
+void logic(int a[], int b){
+    int s = 0;
+    int e = b-1;
+    while(s < e){
+        swap(a[s],a[e]);
+        s++;
+        e--;
+    }
+}
+
+
+int main(){
+    int a[]={1,2,3,4,5,6,7,8};
+    int b = sizeof(a)/sizeof(int);
+    logic(a,b);
+    for(int i=0; i<b; i++){
+        cout << a[i] << " ";
+    }
+}
