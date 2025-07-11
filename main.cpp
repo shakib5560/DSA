c27a83826903f49657589f4f9afea47358bc8f34
shakib5560 <dev.shakib6@gmail.com>
2025-07-11 21:57:39 +0600

    Start 0

---
+++ main.cpp
@@ -0,0 +1,24 @@
+// Linear Search
+
+#include <iostream>
+using namespace std;
+
+int  myarry(int x[], int y, int t) {
+  for (int i = 0; i < y; i++) {
+    if (x[i] == t) {
+      return i; 
+    }
+  }
+  return 0;
+}
+
+int main() {
+  int x[] = {5, 2, 4, 5, 7, 1, 3, 6, 9, 8};
+  int size = sizeof(x) / sizeof(int);
+  int target = 9;
+  if(myarry(x, size, target) == 0){
+    cout << "-1";
+  } else{
+    cout << "index, value: " << myarry(x, size, target) << ", " << x[myarry(x, size, target)];
+  }
+}
